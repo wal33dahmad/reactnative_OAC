899beb2729584ddd6d8730cff298d9e465531a31
Waleed Ahmad <rana.waleed123@gmail.com>
2022-09-25 02:45:12 +0500

    country selector in register screen

--- app/screens/RegisterScreen4.js
+++ app/screens/RegisterScreen4.js
@@ -1,26 +1,44 @@
-import React from "react";
-import { StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, View } from "react-native";
 import * as Yup from "yup";
+import CountryPicker from "react-native-country-picker-modal";
 
 import Screen from "../components/Screen";
 import Text from "../components/Text";
 import { Form, FormField, SubmitButton } from "../components/forms";
+import defaultStyles from "../config/styles";
 
 const validationSchema = Yup.object().shape({
   phone: Yup.number().min(11).required().label("Phone"),
 });
 
 function RegisterScreen({ navigation }) {
+  const [countryCode, setCountryCode] = useState("");
+  const [callingCode, setCallingCode] = useState("");
   return (
     <Screen style={styles.container}>
       <Text style={styles.h1}>Enter your mobile number</Text>
       <Text style={styles.text}>
         Enter the mobile number where you can be reached.
       </Text>
+      <View style={styles.countryContainer}>
+        <CountryPicker
+          withFilter
+          countryCode={countryCode}
+          withFlag
+          withCallingCode
+          withCountryNameButton
+          onSelect={({ callingCode, cca2 }) => {
+            setCallingCode(callingCode);
+            setCountryCode(cca2);
+          }}
+          containerButtonStyle={styles.countrySelector}
+        />
+      </View>
       <Form
         initialValues={{ phone: "" }}
         onSubmit={(values) => {
-          console.log(values);
+          console.log((callingCode[0] + values.phone).toString());
           navigation.push("HomeScreen");
         }}
         validationSchema={validationSchema}
@@ -30,7 +48,7 @@ function RegisterScreen({ navigation }) {
           autoCorrect={false}
           icon="phone"
           name="phone"
-          placeholder="Phone number"
+          placeholder={"Phone number"}
           textContentType="telephoneNumber"
           keyboardType="phone-pad"
         />
@@ -54,6 +72,19 @@ const styles = StyleSheet.create({
     alignSelf: "center",
     fontSize: 14,
     marginBottom: 40,
+  },
+  countryContainer: {
+    backgroundColor: defaultStyles.colors.light,
+    borderRadius: 25,
+    flexDirection: "row",
+    marginVertical: 10,
+    alignItems: "center",
+  },
+  countrySelector: {
+    alignItems: "center",
+    minWidth: "100%",
+    padding: 15,
+    borderRadius: 25,
   },
 });
 
